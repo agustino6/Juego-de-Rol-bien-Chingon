a1ce16920aca2f425f116a7c833f6bc9cf343e2d
Fernando Salcido <fsr313@gmail.com>
2014-11-12 17:53:00 -0700

    correccion final del primer archivo
    xd

--- Sin título 2.py
+++ Sin título 2.py
@@ -12,29 +12,41 @@ defensa=0
 evasion=0
 print "inicia tu epica aventura, tienes 4 habilidades que puedes subir de nivel con un maxicmo de 10 puntos cada una."
 print "al inicio del juego puedes se te otorgan 8 puntos que podras repartir entre las siguientes habilidades:"
-print "1) Vida"
-print "2) defensa"
-print "3) ataque"
-print "4) evasion"
-print "Que niveles quieres subir(tienes que gastar todos los puntos)"
-while puntos_lvl > 0:
+print "a) Vida"
+print "b) defensa"
+print "c) ataque"
+print "d) evasion"
+while puntos_lvl >1 :
     print "Que niveles quieres subir(tienes que gastar todos los puntos)"
-    opcion=raw_input
-    if opcion ==1:
+    opcion=raw_input()
+    if opcion == "a":
         print "cuantos puntos deseas agregar a vida?"
-        puntos_add=raw_input
-        vida =vida+ raw_input
-    elif opcion ==2:
+        puntos_add=int(raw_input())
+        if puntos_add > puntos_lvl:
+            print "valor fuera de rango"
+        vida =vida+puntos_add
+        puntos_lvl=puntos_lvl - puntos_add
+    if opcion =='b':
         print "cuantos puntos deseas agregar a defensa?"
-        puntos_add=raw_input
-        defensa =vida+ raw_input
-    elif opcion== 3:
+        puntos_add=int(raw_input())
+        if puntos_add > puntos_lvl:
+            print "valor fuera de rango"
+        defensa =defensa+ puntos_add
+        puntos_lvl - puntos_add
+    if opcion== 'c':
         print "cuantos puntos deseas agregar a ataque?"
-        puntos_add=raw_input
-        ataque =vida+ raw_input
-    elif opcion ==4:
+        puntos_add=int(raw_input())
+        if puntos_add > puntos_lvl:
+            print "valor fuera de rango"
+        ataque =ataque+ puntos_add
+        puntos_lvl - puntos_add
+    if opcion =='d':
         print "cuantos puntos deseas agregar a evasion?"
-        puntos_add=raw_input
-        evasion =vida+ raw_input
+        puntos_add=int(raw_input())
+        if puntos_add > puntos_lvl:
+            print "valor fuera de rango"
+        evasion =evasion+ puntos_add
+        puntos_lvl - puntos_add
     print "te quedan", puntos_lvl, "puntos para subir de nivel"
+print "fin programa"
     
