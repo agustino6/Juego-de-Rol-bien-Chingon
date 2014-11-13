860cb8ae175811ed6fb12920a9cb40ceefe705d5
Rafael Castillo <savoytruffle321@gmail.com>
2014-11-12 19:18:17 -0700

    Creacion del personaje
    Creacion del personaje lista. juego.py sera el archivo principal a
    ejecutar

---
+++ creacionpersonaje.py
@@ -0,0 +1,38 @@
+import os
+
+def dialogo_puntos(jugador, opc,maximo):
+    stats = ['vida','ataque','defensa','evasion']
+    puntos = int(raw_input(("cuantos puntos deseas agregar a "+stats[opc]+"?[0,"+str(maximo)+"]")))
+    while puntos>maximo:
+        puntos = int(raw_input("valor fuera de rango. Introduce un valor en el rango [0,"+str(maximo)+"]"))
+    jugador[opc] += puntos
+    return maximo - puntos
+
+def escribir_stats(jugador):
+    stats = ['vida','ataque','defensa','evasion','dinero']
+    print 'habilidades del jugador:'
+    for i in range(4):
+        print str(stats[i])+": "+str(jugador[i])
+
+#en orden: vida, defensa, ataque, evasion, dinero.
+def crear_personaje(jugador):
+    while True:
+        print"Selecciona la habilidad que quieres aumentar(debes agotar todos tus puntos):"
+        print "1) Vida (" + str(jugador[0])
+        print "2) Defensa ("+ str(jugador[1])
+        print "3) Ataque (" + str(jugador[2])
+        print "4) Evasion (" + str(jugador[3])
+        opcion=int(raw_input('Elige una opcion'))
+    
+        puntos_lvl = dialogo_puntos(jugador,opcion-1,puntos_lvl)
+        
+        if puntos_lvl>0:
+            print "\nTe quedan", puntos_lvl, "puntos para subir de nivel, tienes que usar todos los puntos."
+        else:
+            print "\nHas terminado de crear a tu personaje."
+            break
+    return jugador
+
+
+
+
---
+++ juego.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 12 18:59:48 2014
+
+@author: Rafael
+"""
+
+from creacionpersonaje import *
+
+def main():
+    puntos_lvl = 8
+    print "inicia tu epica aventura, tienes 4 habilidades que puedes subir de nivel con un maxicmo de 10 puntos cada una.\n"
+    print "al inicio del juego puedes se te otorgan 8 puntos que podras repartir entre las siguientes habilidades(cada habilidad tiene un valor inicial de 5):"
+    print "a) Vida: Si la pierdes toda, te mueres."
+    print "b) Defensa: Reduce el daño que tomas por ataque."
+    print "c) ataque: Aumenta el daño que haces."
+    print "d) Evasion: Aumenta tu probablidad de esquivar un ataque."
+    raw_input('Presiona enter para continuar')
+    os.system('cls')
+    
+    jugador = crear_personaje([5,5,5,5,50])
+    inventario = []
+    os.system('cls')
+    escribir_stats(jugador)
+    
+    #por hacer: TODO
+main()
