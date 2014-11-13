2782a6b06f250b81ad79a1d86217c93c450f7989
Fernando Salcido <fsr313@gmail.com>
2014-11-12 17:14:01 -0700

    el inicio
    del juego

---
+++ Sin título 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 12 16:56:23 2014
+
+@author: Fernando
+"""
+#puntos de nivel
+puntos_lvl = 8
+print "inicia tu epica aventura, tienes 4 habilidades que puedes subir de nivel con un maxicmo de 10 puntos cada una.
+print "al inicio del juego puedes se te otorgan 8 puntos que podras repartir entre las siguientes habilidades:"
+print "1) Vida"
+print "2) defensa"
+print "3) ataque"
+print "4) evasion"
+print "Que niveles quieres subir(tienes que gastar todos los puntos)"
+while puntos_lvl > 0:
+    print "Que niveles quieres subir(tienes que gastar todos los puntos)"
+    opcion
