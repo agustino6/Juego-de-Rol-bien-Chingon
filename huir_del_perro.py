a0bbb52dcaffc77abe035671b61f4f6caf866dab
Franko13 <Francis_e_c_g@hotmail.com>
2014-11-13 21:56:35 -0700

    añadido opc huir del perro
    añadido opc huir del perro

---
+++ huir_del_perro.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 13 21:47:39 2014
+
+@author: Fran
+"""
+
+
+def huir_del_perro():
+
+ print "Has seleccionado huir del perro"
+
+ print """
+ Tus charolas empiezan a sonar por la banqueta
+ pero no hacía la tienda, si no, lejos del perro. 
+ Sigues corriendo y el perro aun no deja de perseguirte!,
+ de pronto ves a lo lejos a doña Cuquita te acercas un poco
+ más y ella por fin te reconoce y te saluda con una sonrisa
+ como siempre. Pasas por un lado de ella y el perro se lanza 
+ sobre ella! dándole un fin al corrido de doña Cuquita(Rip).
+ Pero por otro lado, saliste con vida, aun puedes ir por la 
+ leche y vivir tu vida. Pero perdiste 15 pesos mientras corrías"
+ """
+ #money -= 15
+ 
+ 
+ 
+ 
+ 
+ 
