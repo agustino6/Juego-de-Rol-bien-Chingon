d10ac9f85aa9dc02b6ec32011a4b84aa97c406b4
Rafael Castillo <savoytruffle321@gmail.com>
2014-11-15 01:33:59 -0700

    arreglar mensajes

--- creacionpersonaje.py
+++ creacionpersonaje.py
@@ -9,8 +9,8 @@ def dialogo_puntos(jugador, opc,maximo):
     return maximo - puntos
 
 def escribir_stats(jugador):
-    stats = ['vida','ataque','defensa','evasion','dinero']
-    print 'habilidades del jugador:'
+    stats = ['Vida','Ataque','Defensa','Evasion','Dinero']
+    print 'Habilidades del jugador:'
     for i in range(4):
         print str(stats[i])+": "+str(jugador[i])
 
@@ -18,11 +18,11 @@ def escribir_stats(jugador):
 def crear_personaje(jugador,puntos_lvl):
     while True:
         print"Selecciona la habilidad que quieres aumentar(debes agotar todos tus puntos):"
-        print "1) Vida (" + str(jugador[0])
-        print "2) Defensa ("+ str(jugador[1])
-        print "3) Ataque (" + str(jugador[2])
-        print "4) Evasion (" + str(jugador[3])
-        opcion=int(raw_input('Elige una opcion'))
+        print "1) Vida (" + str(jugador[0])+")"
+        print "2) Defensa ("+ str(jugador[1])+")"
+        print "3) Ataque (" + str(jugador[2])+")"
+        print "4) Evasion (" + str(jugador[3])+")"
+        opcion=int(raw_input('Elige una opcion: '))
     
         puntos_lvl = dialogo_puntos(jugador,opcion-1,puntos_lvl)
         
